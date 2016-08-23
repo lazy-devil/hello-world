cbe3f8885fcae6bd58c9bd7091fdbed02a683851
lazy-devil <niran.p@hotmail.com>
2016-08-23 13:40:32 +0700

    Update 2

--- HelloWordConsoleApplication/HelloWordConsoleApplication/Program.cs
+++ HelloWordConsoleApplication/HelloWordConsoleApplication/Program.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace HelloWordConsoleApplication
+namespace HelloWorldConsoleApplication
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!!!");
         }
     }
 }
