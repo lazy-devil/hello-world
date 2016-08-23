5210b12066cce90504540ed06aa7ffa67ce619d2
lazy-devil <niran.p@hotmail.com>
2016-08-23 13:45:09 +0700

    Test comment

--- HelloWordConsoleApplication/HelloWordConsoleApplication/Program.cs
+++ HelloWordConsoleApplication/HelloWordConsoleApplication/Program.cs
@@ -10,6 +10,7 @@ namespace HelloWorldConsoleApplication
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!!!");
+            // Test comment
         }
     }
 }
